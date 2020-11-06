42d471956a45a385b2a45e859874746ed1dcf535
ALFI22-01 <72850530+ALFI22-01@users.noreply.github.com>
2020-11-06 23:21:54 +0700

    Project 40

---
+++ src/latihan40/warnakepribadian/oo/Latihan40WarnaKepribadianOO.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan40.warnakepribadian.oo;
+
+/**
+ *
+ * @author Alfi Nurizkya
+ * NAMA     : ALFI NURIZKYA
+ * KELAS    : IF-1
+ * NIM      : 10119036
+ * DESKRIPSI PROGRAM : Membuat Data Warna Kepribadian Berbasis Object Oriented
+ */
+
+
+public class Latihan40WarnaKepribadianOO {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Warna wrn = new Warna();
+        wrn.inputWarna();
+        wrn.inputNama();
+        wrn.cekWarna();
+    }
+    
+}
